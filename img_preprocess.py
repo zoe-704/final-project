2b3a7c8d7bbb65b680a06eb66d06a77853772149
zoe-704 <sunzoey@gmail.com>
2026-04-14 11:20:52 -0700

    image preprocessing

---
+++ img_preprocess.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def preprocess_image(image_path, target_size=(224, 224)):
+    # 1. load image
+    img = cv2.imread(image_path)
+    # 2. resize
+    img = cv2.resize(img, target_size)
+    # 3. normalize
+    img = img.astype('float_32') / 255.0
+    # 4. reshape ????
+    img = np.expand_dims(img, axis=0)
+    
+    return img
