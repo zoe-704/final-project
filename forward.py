6f8ed884d4d58417c111474f2c1cf0f18ae0fab2
zoe-704 <sunzoey@gmail.com>
2026-04-21 15:55:55 -0700

    flatten, dense layer, softmax, cross entropy loss

--- forward.py
+++ forward.py
@@ -1,8 +1,10 @@
 import numpy as np
 
+    # output is 4d tensor (batch_size, out_channels, out_height, out_width)
+    # input (1, 1, 64, 64) & kernel (8, 1, 3, 3) --> output (1, 8, 62, 62)
     def conv2D(input, kernel, stride=1, padding=0): 
         batch_size, in_depth, in_height, in_width = input.shape
-        out_channels, kernel_height, kernel_width = kernel.shape[0], kernel.shape[2], kernel.shape[3]
+        out_channels, __, kernel_height, kernel_width = kernel.shape
 
         # pad input before applying convolution filter without changing batch size and channel dimensions
         input_padded = np.pad(input, ((0, 0), (0, 0), (padding, padding), (padding, padding)), mode='constant')
@@ -15,11 +17,9 @@ import numpy as np
         for n in range(batch_size): 
             for i in range(out_height):
                 for j in range(out_width):
-                    # kernel applied for each filter for each position in input
-                    for c in range(out_channels):
-                        # dot product multiplies element in region with element in the kernel and sums all values
-                        region = input_padded[n, :, i * stride:i * stride + kernel_height, j * stride:j * stride + kernel_width]
-                        output[n, c, i, j] = np.sum(region * kernel[c])
+                    # dot product multiplies element in region with element in the kernel and sums all values
+                    region = input_padded[n, :, i * stride:i * stride + kernel_height, j * stride:j * stride + kernel_width]
+                    output[n, c, i, j] = np.sum(region * kernel[c])
         return output
 
     def relu(x):
@@ -27,8 +27,8 @@ import numpy as np
     
     def max_pooling(input, pool_size, stride):
         batch_size, in_depth, in_height, in_width = input.shape
-        out_height = in_height - pool_size // stride + 1
-        out_width = in_width - pool_size // stride + 1
+        out_height = (in_height - pool_size) // stride + 1
+        out_width = (in_width - pool_size) // stride + 1
         output = np.zeros(batch_size, in_depth, out_height, out_width)
         # iterate through h and w of feature map for each image
         for n in range(batch_size): 
@@ -36,5 +36,26 @@ import numpy as np
                 for j in range(out_width):
                         region = input_padded[n, :, i * stride:i * stride + pool_size, j * stride:j * stride + pool_size]
                         # find the max value of each region (most significant feature of area)
-                        output[n, c, i, j] = np.max(region, axis=(1, 2))
+                        output[n, c, i, j] = np.max(region)
         return output
+
+    # transform 4d tensor (N, C, H, W) --> (N, features)
+    def flatten(x):
+        return x.reshape(x.shape[0], -1)
+    
+    # weighted sum of all input features and bias
+    def dense(x, W, b):
+        return x @ W + b
+
+    # equation: softmax(x_i) = e^(x_i) / sum of [e^(x_j)]
+    def softmax(x):
+        exp = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return exp / np.sum(exp, axis=1, keepdims=True)
+
+    # equation: L = -(1/N) * sum of [log(probs_i)]
+    def cross_entropy(pred, labels):
+        N = pred.shape[0]
+        probs = pred[np.arrange(N), labels]
+        loss = -np.mean(np.log(probs + 1e8))
+        return loss
+        
