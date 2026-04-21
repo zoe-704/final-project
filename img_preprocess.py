c0a237c95221fbb41f0f8a1bf0b4d988a5c3b31d
zoe-704 <sunzoey@gmail.com>
2026-04-21 09:28:57 -0700

    update img preprocessing for grayscale

--- img_preprocess.py
+++ img_preprocess.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 
 def preprocess_image(image_path, target_size=(64, 64)):
     # 1. load image
-    img = cv2.imread(image_path)
-    # 2. resize
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    # 2. resize and
     img = cv2.resize(img, target_size)
-    # 3. normalize
-    img = img.astype('float_32') / 255.0
-    # 4. reshape ????
-    img = np.expand_dims(img, axis=0)
     
+    img = img.astype('float_32') / 255.0
+
+    # 4. add channel dimension -> (1, H, W)
+    img = np.expand_dims(img, axis=0)
+    # 5. add batch dimension -> (1, 1, H, W)
+    img = np.expand_dims(img, axis=0)
+
     return img
